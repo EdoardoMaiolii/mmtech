1e5b5e59f02a5f701545fde296af98a4bc340984
kevin mancini <kevin.mancini@studio.unibo.it>
2020-12-22 17:27:22 +0100

    add profile

---
+++ template/profile-section1.php
@@ -0,0 +1,1 @@
+<h1>  DATI PERSONALI</h1>
--- template/profile.php
+++ template/profile.php
@@ -1,7 +1,13 @@
 <nav>
-  <ul>
-    <li><a href="#">Dati Personali</a></li>
-    <li><a href="#">I miei ordini</a></li>
-    <li><a href="#">Esci</a></li>
-  </ul>
+    <ul>
+        <li><a href="profile.php?profile-section=1">Dati Personali</a></li>
+        <li><a href="profile.php?profile-section=2">I miei ordini</a></li>
+        <li><a href="profile.php?profile-section=3">Esci</a></li>
+    </ul>
+
+    <?php
+    if (isset($templateParams["profile-section"])) {
+        require("profile-section" . $templateParams["profile-section"] . "php");
+    }
+    ?>
 </nav>
