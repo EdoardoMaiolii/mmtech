44b546ca2309df33766004845bc66cf9db33c1bd
unknown <edoardo.maioli@studio.unibo.it>
2020-12-30 18:14:14 +0100

    cart finished

--- template/cart-section.php
+++ template/cart-section.php
@@ -1,21 +1,20 @@
 <section>
     <h1>Prodotti nel carrello</h1>
+    <form action="updatecart.php" method="GET">
     <?php foreach ($templateParams['cartproducts'] as $product) : ?>
         <article>
             <h2> <?php echo $product['nome']; ?> </h2>
             <a href="product.php?productid=<?php echo (int) UPLOAD_DIR.$product["idprodotto"]; ?>"><img src="<?php echo UPLOAD_DIR.$product["nomeimmagine"]; ?>" alt="" /></a>
-            <form action="updatecart.php" method="GET">
-            <input type="hidden" name="idprodotto" id="idprodotto" value=<?php echo $product['idprodotto']; ?>/>
-            <select id = "quantita" name = "quantita" onchange="submitQuantityBtn();" value = <?php echo $product['quantita']; ?>>
+            <select id = <?php echo 'quantita-'.$product['idprodotto'] ?> name =<?php echo 'quantita-'.$product['idprodotto']?> onchange="submitQuantityBtn();" value = <?php echo $product['quantita']; ?>>
                 <?php for($i=1;$i<10;$i++): ?>
                     <option <?php if($product['quantita'] == $i) echo 'selected = selected'; echo 'value = '.$i ?> ><?php echo $i; ?></option>
                 <?php endfor; ?>
-                <input type ="submit" id="updateQuantity" name="updateQuantity" hidden="hidden"/>
             </select>
             <a href="updatecart.php?action=1&idprodotto=<?php echo $product["idprodotto"] ?>">Rimuovi articolo dal carrello</a>
-            </form>
         </article>
     <?php endforeach; ?>
+    <input type ="submit" id="updateQuantity" name="updateQuantity" hidden="hidden"/>
+    </form>
     </br>
     <form action="updatecart.php" method ="GET">
         <input type="hidden" id="order" name="order" value ="true">
--- updatecart.php
+++ updatecart.php
@@ -1,14 +1,16 @@
 <?php
 require_once("bootstrap.php");
-
+$templateParams["cartproducts"] = $dbh->getCartProducts($_SESSION['email']);
 if(isset($_GET['order'])){
     $dbh->addOrder($_SESSION['email']);
 }
 if (isset($_GET['action']) && isset($_GET['idprodotto'])) {
     $dbh->removeProductFromCart($_SESSION['email'], (int)$_GET['idprodotto']);
 }
-if (isset($_GET['quantita'])) {
-    $dbh->updateQuantity($_GET['quantita'], $_SESSION['email'], (int)$_GET['idprodotto']);
+foreach($templateParams["cartproducts"] as $product){
+    if(isset($_GET['quantita-'.$product['idprodotto']]) && $_GET['quantita-'.$product['idprodotto']] != $product['quantita'] ){
+        $dbh->updateQuantity($_GET['quantita-'.$product['idprodotto']], $_SESSION['email'], $product['idprodotto']);
+    }
 }
 $templateParams["title"] = "Carrello -" . $_SESSION['nome'];
 $templateParams["content"] = "cart-section.php";
