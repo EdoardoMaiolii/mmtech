21625aa189031222778a691ca9c1b1c6a7f85378
kevin mancini <kevin.mancini@studio.unibo.it>
2021-01-16 17:34:27 +0100

    update settings design

--- template/settings-section1.php
+++ template/settings-section1.php
@@ -1,38 +1,40 @@
 <section class="bg-light">
     <h1> GESTIONE PRODOTTI</h1>
     <div id="blackline"></div>
-    <h2> Inserisci un nuovo prodotto </h2>
-    <form class="form-inline" enctype="multipart/form-data" id="form1" action="settings.php" method="POST">
+    <p id="ppar"> Inserisci un nuovo prodotto </p>
+    <div id="containerMid">
+    <form enctype="multipart/form-data" id="form1" action="settings.php" method="POST">
         <fieldset>
             <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="nomeProdotto">Nome Prodotto: </label><input class="form-control" type="text" id="nomeProdotto" name="nomeProdotto" /> </br>
+                <label id="info" class="col-form-label" for="nomeProdotto">Nome Prodotto: </label><input class="form-control" type="text" id="nomeProdotto" name="nomeProdotto" /> </br>
             </div>
             <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="imgProdotto">Immagine Prodotto: </label><input class="form-control" type="file" name="imgProdotto" id="imgProdotto" accept="image/png, image/jpeg, image/jpg, image/gif" /> </br>
+                <label id="info" class="col-form-label" for="imgProdotto">Immagine Prodotto: </label><input class="form-control" type="file" name="imgProdotto" id="imgProdotto" accept="image/png, image/jpeg, image/jpg, image/gif" /> </br>
             </div>
-            <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="categoria">Categoria: </label><select class="custom-select" id="categoria" name="categoria" ?>
+            <div id="mediumrow" class="form-group">
+                <label id="info" class="col-form-label" for="categoria">Categoria: </label><select class="custom-select" id="categoria" name="categoria" ?>
                     <?php foreach ($templateParams["categories"] as $category) : ?>
                         <option <?php echo 'value = ' . $category['nomecategoria'] ?>> <?php echo $category['nomecategoria'] ?></option>
                     <?php endforeach; ?>
                 </select></br>
             </div>
-            <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="costo">Costo: </label><input class="form-control" type="text" id="costo" name="costo" /> </br>
+            <div id="minirow" class="form-group">
+                <label id="info" class="col-form-label" for="costo">Costo: </label><input class="form-control" type="text" id="costo" name="costo" /> </br>
+            </div>
+            <div id="minirow" class="form-group">
+                <label id="info" class="col-form-label" for="costoSpedizione">Costo Spedizione: </label><input class="form-control" type="text" id="costoSpedizione" name="costoSpedizione" /> </br>
+            </div>
+            <div id="minirow" class="form-group">
+                <label id="info" class="col-form-label" for="quantitaDisponibile">Quantita' disponibile: </label><input class="form-control" type="text" id="quantitaDisponibile" name="quantitaDisponibile" /> </br>
             </div>
             <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="costoSpedizione">Costo Spedizione: </label><input class="form-control" type="text" id="costoSpedizione" name="costoSpedizione" /> </br>
+                <label id="info" class="col-form-label" for="descrizione">Descrizione: </label><textarea class="form-control" name="descrizione" form="form1" placeholder="Inserisci la descrizione qui..."></textarea></br>
             </div>
             <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="quantitaDisponibile">Quantita' disponibile: </label><input class="form-control" type="text" id="quantitaDisponibile" name="quantitaDisponibile" /> </br>
-            </div>
-            <div id="row" class="form-group">
-                <label id="info" class="col-sm-2 col-form-label" for="descrizione">Descrizione: </label><textarea class="form-control" name="descrizione" form="form1" placeholder="Inserisci la descrizione qui..."></textarea></br>
-            </div>
-            <div id="row" class="form-group col-sm-10">
-                <button class="btn btn-primary" type="submit" id="insertProduct" name="insertProduct"> Inserisci il prodotto </button></br>
+                <button id="btn" class="btn btn-primary" type="submit" id="insertProduct" name="insertProduct"> Inserisci il prodotto </button></br>
             </div>
         </fieldset>
     </form>
+    </div>
     <p id="ppar"> <strong>Se si vuole modificare o eliminare un prodotto e' possibile farlo cercando il medesimo con la barra di ricerca oppure con il menu' laterale </strong> </p>
 </section>
--- template/settings.php
+++ template/settings.php
@@ -1,7 +1,7 @@
 <div id="content">
 <link rel="stylesheet" type="text/css" href="./css/profile-settings.css" />
-<nav id="sidebar" data-toggle="collapse" class="navbar navbar-expand-lg navbar-light bg-light">
-    <ul class="nav nav nav-tabs nav-fill">
+<nav>
+    <ul class="nav nav-tabs">
         <li class="nav-item"><a <?php echo ((!isset($templateParams["settings-section"]) || $templateParams["settings-section"] == "1") ? 'class="nav-link active"' : 'class="nav-link"') ?>  href="settings.php?settings-section=1">Gestione Prodotti</a></li>
         <li class="nav-item"><a <?php echo (($templateParams["settings-section"] == "2") ? 'class="nav-link active"' : 'class="nav-link"') ?>href="settings.php?settings-section=2">Gestione Ordini</a></li>
     </ul>
