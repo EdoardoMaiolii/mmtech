c052dd1e7c6e483a5a476657bc3fcc01e3cdd4ad
Elia <elia.urbini@studio.unibo.it>
2021-01-17 16:53:18 +0100

    add rgb

--- template/home.php
+++ template/home.php
@@ -3,14 +3,13 @@
     <div id="blackline"></div>
     <div class="row no-gutters">
         <?php foreach ($templateParams["mostviewed"] as $product) : ?>
-            <div id="box" class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-5 pl-5 gradient-border">
-                <figure>
+            <div id="box" class="product-img">
+                <figure class="gradient-border" id="box">
                     <a href="product.php?productid=<?php echo $product["idprodotto"]; ?>"> <img class="homeImage" src="<?php echo UPLOAD_DIR . $product["nomeimmagine"]; ?>" alt=<?php echo $product["nome"]; ?> /></a>
                     <figcaption id="info"> <?php echo $product["nome"]; ?> </figcaption>
                 </figure>
             </div>
         <?php endforeach; ?>
-    </div>
 </section>
 <section id="Chronology">
     <?php if (isset($templateParams["chrono"])) : ?>
@@ -18,7 +17,7 @@
         <div id="blackline"></div>
         <div class="row no-gutters">
             <?php foreach ($templateParams["chrono"] as $product) : ?>
-                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-5 pl-5">
+                <div class="product-img">
                     <figure>
                         <a href="product.php?productid=<?php echo $product["idprodotto"]; ?>"> <img class="homeImage" src="<?php echo UPLOAD_DIR . $product["nomeimmagine"]; ?>" alt=<?php echo $product["nome"]; ?> /></a>
                         <figcaption> <?php echo $product["nome"]; ?> </figcaption>
