87820cbad02172c6afbe48418650f19c81d0cf7f
Elia <elia.urbini@studio.unibo.it>
2021-01-17 16:02:23 +0100

    rgm site

--- template/home.php
+++ template/home.php
@@ -3,7 +3,7 @@
     <div id="blackline"></div>
     <div class="row no-gutters">
         <?php foreach ($templateParams["mostviewed"] as $product) : ?>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-5 pl-5 photo-wrapper">
+            <div id="box" class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-5 pl-5 gradient-border">
                 <figure>
                     <a href="product.php?productid=<?php echo $product["idprodotto"]; ?>"> <img class="homeImage" src="<?php echo UPLOAD_DIR . $product["nomeimmagine"]; ?>" alt=<?php echo $product["nome"]; ?> /></a>
                     <figcaption id="info"> <?php echo $product["nome"]; ?> </figcaption>
